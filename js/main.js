1b1ff783159a6f3364a9df32e723b8625d63dda0
moamel-hashim <muamal1997@gmail.com>
2023-08-09 12:24:28 -0700

    completed adding ajax request

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 const $ = require('jquery');
 $(document).ready(function () {
+
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: 'https://dad-jokes.p.rapidapi.com/random/joke',
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '19cde8be53mshba709677a7fe40ep101cbcjsnf2989692780b',
+      'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
+    }
+  };
+
+  $.ajax(settings).done(function (response) {
+    const setup = response.body[0].setup;
+    const punchline = response.body[0].punchline;
+    $('.jokes').text(`${setup} ${punchline}`);
+    dadJokesArray.push(`${setup} ${punchline}`);
+  });
   $('button.emoji').hover(
     function () {
       $(this).css({ transform: 'scale(1.2)', transition: 'transform 0.5s' });
@@ -8,8 +26,8 @@ $(document).ready(function () {
       $(this).css({ transform: 'scale(1)', transition: 'transform 0.5s' });
     }
   );
-});
-
-$('.emoji-container').on('click', function () {
-  $(this).addClass('hide');
+  $('.emoji-container').on('click', function () {
+    $(this).addClass('hide');
+  });
+  const dadJokesArray = [];
 });
