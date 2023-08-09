8f49a6e1cb41b091a2fd8b516f0ef3eef27169a8
moamel-hashim <muamal1997@gmail.com>
2023-08-09 13:58:57 -0700

    completed adding right and left errows

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
 const $ = require('jquery');
 $(document).ready(function () {
 
-  const settings = {
-    async: true,
-    crossDomain: true,
-    url: 'https://dad-jokes.p.rapidapi.com/random/joke',
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '19cde8be53mshba709677a7fe40ep101cbcjsnf2989692780b',
-      'X-RapidAPI-Host': 'dad-jokes.p.rapidapi.com'
-    }
-  };
-
-  $.ajax(settings).done(function (response) {
-    const setup = response.body[0].setup;
-    const punchline = response.body[0].punchline;
-    $('.jokes').text(`${setup} ${punchline}`);
-    dadJokesArray.push(`${setup} ${punchline}`);
-  });
   $('button.emoji').hover(
     function () {
       $(this).css({ transform: 'scale(1.2)', transition: 'transform 0.5s' });
@@ -28,6 +11,25 @@ $(document).ready(function () {
   );
   $('.emoji-container').on('click', function () {
     $(this).addClass('hide');
+
+    const settings = {
+      async: true,
+      crossDomain: true,
+      url: 'https://icanhazdadjoke.com/',
+      method: 'GET',
+      dataType: 'json',
+      headers: {
+        Accept: 'application/json'
+      }
+    };
+
+    $.ajax(settings).done(function (response) {
+      const dadJoke = response.joke;
+      $('.jokes').text(`${dadJoke}`);
+      dadJokesArray.push(`${dadJoke}`);
+      console.log(response);
+    });
   });
+
   const dadJokesArray = [];
 });
