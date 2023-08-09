0a7ec5915a600da31f7c7670e872832968f98711
moamel-hashim <muamal1997@gmail.com>
2023-08-09 14:57:16 -0700

    finished up adding carousel

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 const $ = require('jquery');
 $(document).ready(function () {
+  const settings = {
+    async: true,
+    crossDomain: true,
+    url: 'https://icanhazdadjoke.com/',
+    method: 'GET',
+    dataType: 'json',
+    headers: {
+      Accept: 'application/json'
+    }
+  };
 
+  $.ajax(settings).done(function (response) {
+    const dadJoke = response.joke;
+    $('.jokes').text(`${dadJoke}`);
+    dadJokesArray.push(dadJoke);
+  });
   $('button.emoji').hover(
     function () {
       $(this).css({ transform: 'scale(1.2)', transition: 'transform 0.5s' });
@@ -11,25 +26,29 @@ $(document).ready(function () {
   );
   $('.emoji-container').on('click', function () {
     $(this).addClass('hide');
+  });
 
-    const settings = {
-      async: true,
-      crossDomain: true,
-      url: 'https://icanhazdadjoke.com/',
-      method: 'GET',
-      dataType: 'json',
-      headers: {
-        Accept: 'application/json'
-      }
-    };
+  $('.fa-chevron-right').on('click', function () {
+    dadJokesIndex++;
+    if (dadJokesArray[dadJokesIndex] === undefined) {
+      $.ajax(settings).done(function (response) {
+        const dadJoke = response.joke;
+        $('.jokes').text(`${dadJoke}`);
+        dadJokesArray.push(dadJoke);
+      });
+    } else {
+      $('.jokes').text(dadJokesArray[dadJokesIndex]);
+    }
+  });
 
-    $.ajax(settings).done(function (response) {
-      const dadJoke = response.joke;
-      $('.jokes').text(`${dadJoke}`);
-      dadJokesArray.push(`${dadJoke}`);
-      console.log(response);
-    });
+  $('.fa-chevron-left').on('click', function () {
+    if (dadJokesIndex > 0) {
+      dadJokesIndex--;
+      $('.jokes').text(dadJokesArray[dadJokesIndex]);
+    }
   });
 
   const dadJokesArray = [];
+  let dadJokesIndex = -1;
+  console.log('value of dadjokesarray', dadJokesArray);
 });
